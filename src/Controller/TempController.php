9d28a93280483c4fbc78ebce7c404c2bdbfddabb
SarraGobjii <sarra.gobji2@gmail.com>
2026-01-05 16:38:31 +0100

    redirect login page too user template according to role assigned

---
+++ src/Controller/TempController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TempController extends AbstractController
+{
+    #[Route('/user-temp', name: 'user_temp')]
+    public function userTemp(): Response
+    {
+        return new Response('<h1>User temporary dashboard</h1>');
+    }
+
+    #[Route('/admin-temp', name: 'admin_temp')]
+    public function adminTemp(): Response
+    {
+        return new Response('<h1>Admin temporary dashboard</h1>');
+    }
+}
